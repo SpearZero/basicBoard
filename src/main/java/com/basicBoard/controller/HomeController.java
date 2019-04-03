fa4da312e585d6cd734048c6f4bec992772885c1
SpearZero <spearzero0@gmail.com>
2019-04-03 16:12:01 +0900

    회원가입 페이지 주소, 컨트롤러  변경
    회원가입 페이지 주소를 HomeController를 통해 접속하게 변경

--- src/main/java/com/basicBoard/controller/HomeController.java
+++ src/main/java/com/basicBoard/controller/HomeController.java
@@ -8,6 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -17,5 +18,11 @@ public class HomeController {
 	public String home() {
 		
 		return "home";
+	}
+	
+	@RequestMapping(value = "/register", method = RequestMethod.GET)
+	public String signup() {
+		
+		return "signup";
 	}
 }
